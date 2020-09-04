fb23c0807a542e5fd1f5b9f181f66b23f786ffb5
Umar Ahmed T <64320803+UmarAhmed30@users.noreply.github.com>
2020-09-05 04:08:00 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/settings_screen.dart';
+import 'screens/smart_devices_screen.dart';
+import 'screens/payment_screen.dart';
+import 'screens/schedule_payment_screen.dart';
+import 'screens/payment_history_screen.dart';
+
+void main() {
+  runApp(PaymentHistoryScreen());
+}
